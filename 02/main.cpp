1d2370425d9e6f92e6a370d1ea3810e635564ce2
schnell-rms <schnell.remus@gmail.com>
2024-12-02 21:09:59 +0100

    Day 2: second star

--- 02/main.cpp
+++ 02/main.cpp
@@ -4,11 +4,64 @@
 
 #include <utils.h>
 #include <string>
-#include <unordered_map>
+#include <stack>
 
 #include <cassert>
 
 using namespace std;
+
+
+std::tuple<bool, TNumber> isOkTransition (TNumber prevLevel, TNumber currentLevel) {
+    TNumber diff = currentLevel - prevLevel;
+    TNumber sign = (diff > 0) ? 1
+                            : (diff == 0) ? 0 : -1;
+    // Equality checked by the first condition. No need to check sign fo 0.
+    return {(diff != 0) && (abs(diff) < 4), sign};
+}
+
+bool isValid(const std::vector<TNumber> &numbers, size_t i, std::stack<TNumber> &sequence, TNumber &prevSign) {
+
+    if (i == numbers.size()) {
+        return sequence.size() + 1 >= numbers.size();
+    }
+
+    if (sequence.size() + 1 < i) {
+        // more than one failure accumulated already
+        return false;
+    }
+
+    bool isPushed = true;
+    if (sequence.empty()) {
+        sequence.push(numbers[i]);
+    } else {
+        TNumber prev = sequence.top();
+        auto [isOk, sign] = isOkTransition(prev, numbers[i]);
+
+        if (isOk && (sequence.size() == 1)) {
+            assert(sign != 0);
+            prevSign = sign;
+        }
+
+        if (isOk && (prevSign == sign)) {
+            //Ok
+            sequence.push(numbers[i]);
+        } else {
+            // jump over the current element at position i
+            isPushed = false;
+        }
+    }
+
+    if (isValid(numbers, i+1, sequence, prevSign)) {
+        return true;
+    }
+
+    if (isPushed) {
+        sequence.pop();
+        return isValid(numbers, i+1, sequence, prevSign);
+    }
+
+    return false;
+};
 
 int main(int argc, char *argv[]) {
 
@@ -43,17 +96,23 @@ int main(int argc, char *argv[]) {
         return true;
     };
 
+
     std::string line;
     std::vector<std::string> nodeNames;
-    int nbSafe = 0;
+    int nbSafe1 = 0;
+    int nbSafe2 = 0;
     while(getline(listFile, line)) {
         if (!line.empty()) {
             auto numbers = allNumbers(line);
+            nbSafe1 += isSafe(numbers);
 
-            nbSafe += isSafe(numbers);
+            std::stack<TNumber> sequence;
+            TNumber prevSign = 0;
+            nbSafe2 += isValid(numbers, 0, sequence, prevSign);;
 
         }
     }
   
-    cout << "Safe lines " << nbSafe << endl;
+    cout << "Safe lines 1 " << nbSafe1 << endl;
+    cout << "Safe lines 2 " << nbSafe2 << endl;
 }
