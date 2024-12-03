b120e45ada002622df39746914875c8b1f6c6796
schnell-rms <schnell.remus@gmail.com>
2024-12-03 09:00:33 +0100

    Day 03: second star

--- 03/main.cpp
+++ 03/main.cpp
@@ -14,10 +14,32 @@ TNumber mulSum(const std::string& line) {
     std::vector<TNumber> ret;
     std::string::const_iterator searchStart(line.cbegin() );
     while (std::regex_search(searchStart, line.cend(), sm, std::regex("mul\\((\\d|\\d{2}|\\d{3}),(\\d|\\d{2}|\\d{3})\\)"))) {
-
         const TNumber p1 = std::stol(sm[1]);
         const TNumber p2 = std::stol(sm[2]);
         sum += p1 * p2;
+
+        searchStart = sm.suffix().first;
+    }
+    return sum;
+};
+
+TNumber mulSum2(const std::string& line) {
+    static bool isActive = true;
+    TNumber sum = 0;
+    std::smatch sm;
+    std::vector<TNumber> ret;
+    std::string::const_iterator searchStart(line.cbegin() );
+    while (std::regex_search(searchStart, line.cend(), sm, std::regex("mul\\((\\d|\\d{2}|\\d{3}),(\\d|\\d{2}|\\d{3})\\)|do\\(\\)|don't\\(\\)"))) {
+
+        if (sm[0] == "do()") {
+            isActive = true;
+        } else if (sm[0] == "don't()") {
+            isActive = false;
+        } else if (isActive) {
+            const TNumber p1 = std::stol(sm[1]);
+            const TNumber p2 = std::stol(sm[2]);
+            sum += p1 * p2;
+        }
 
         searchStart = sm.suffix().first;
     }
@@ -45,11 +67,14 @@ int main(int argc, char *argv[]) {
     std::string line;
     std::vector<std::string> nodeNames;
     TNumber sum = 0;
+    TNumber sum2 = 0;
     while(getline(listFile, line)) {
         if (!line.empty()) {
             sum += mulSum(line);
+            sum2 += mulSum2(line);
         }
     }
 
     cout << "Sum is " << sum << endl;
+    cout << "Sum2 is " << sum2 << endl;
 }
