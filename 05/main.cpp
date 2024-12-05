cc2528c6237e210878854eb4892f15ad5f917bab
schnell-rms <schnell.remus@gmail.com>
2024-12-05 07:30:57 +0100

    Day 05: second star

--- 05/main.cpp
+++ 05/main.cpp
@@ -16,17 +16,40 @@ bool isInOrder( const std::vector<std::vector<TNumber>> &rules,
         orderMap[order[idx]] = idx;
     }
 
+    bool isInOrder = true;
+
     for (auto rule: rules) {
         auto itBefore = orderMap.find(rule[0]);
         auto itAfter = orderMap.find(rule[1]);
         if (itBefore != orderMap.end() && itAfter != orderMap.end()) {
             if (itAfter->second < itBefore->second) {
-                return false;
+                isInOrder = false;
+                break;
             }
         }
     }
 
-    return true;
+    if (!isInOrder) {
+        std::unordered_map<TNumber, std::vector<TNumber>> rulesMap;
+        for (auto rule: rules) {
+            rulesMap[rule[0]].push_back(rule[1]);
+        }
+
+        auto orderRule = [&rulesMap](TNumber nBefore, TNumber nAfter) {
+            // Is there a rule defining pages to be after nAfter?
+            auto ruleIt = rulesMap.find(nAfter);
+            if (ruleIt == rulesMap.end())
+                return true; // no rule, everything in order
+
+            // Does that rule require nBefore to be after nAfter?
+            auto it = std::find(ruleIt->second.begin(), ruleIt->second.end(), nBefore);
+            return it == ruleIt->second.end(); // nBefore not found, no such rule, return true.
+        };
+
+        std::sort(order.begin(), order.end(), orderRule);
+    }
+
+    return isInOrder;
 }
 
 int main(int argc, char *argv[]) {
@@ -51,7 +74,8 @@ int main(int argc, char *argv[]) {
     std::string line;
     bool isRule = true;
     std::vector<std::vector<TNumber>> rules;
-    TNumber sum = 0;
+    TNumber sumInOrder = 0;
+    TNumber sumNotInOrder = 0;
     while(getline(listFile, line)) {
         if (!line.empty()) {
             if (isRule) {
@@ -59,7 +83,9 @@ int main(int argc, char *argv[]) {
             } else {
                 auto order = allNumbers(line);
                 if (isInOrder(rules, order)) {
-                    sum += order[order.size()/2];
+                    sumInOrder += order[order.size()/2];
+                } else {
+                    sumNotInOrder += order[order.size()/2];
                 }
             }
         } else {
@@ -67,5 +93,6 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    cout << "Sum is " << sum << endl;
+    cout << "Sum in order is " << sumInOrder << endl;
+    cout << "Sum NOT in order is " << sumNotInOrder << endl;
 }
