3fd910c18e05952cd4a424e5aeed9399f86f4a78
schnell-rms <schnell.remus@gmail.com>
2024-12-26 02:16:52 +0200

    Day19: second star

--- 19/main.cpp
+++ 19/main.cpp
@@ -22,6 +22,37 @@ bool isPossible(std::string_view target, const std::vector<std::string> &pattern
     return false;
 }
 
+// from what index, how many ways
+using TMemory = std::unordered_map<TNumber, TNumber>;
+
+TNumber countPossibilities( std::string_view target,
+                            TNumber index,
+                            const std::vector<std::string> &patterns,
+                            TMemory &memory) {
+
+    if (target.empty()) {
+        return 1;
+    }
+
+    auto it = memory.find(index);
+    if (it != memory.end()) {
+        return it->second;
+    }
+
+    TNumber counter = 0;
+    for (auto &p : patterns) {
+        if (target.starts_with(p)) {
+            counter += countPossibilities(  target.substr(p.size()),
+                                            index + p.size(),
+                                            patterns,
+                                            memory);
+        }
+    }
+
+    memory[index] = counter;
+    return counter;
+}
+
 int main(int argc, char *argv[]) {
 
     gIS_DEBUG = argc > 2;
@@ -56,12 +87,20 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    // First star:
+    // Both stars:
     TNumber first = 0;
+    TNumber second = 0;
+    TNumber counter;
+    TMemory memory;
     for (auto target:targets) {
-        first += isPossible(target, patterns);
+        // first += isPossible(target, patterns);
+        memory.clear();
+        counter = countPossibilities(target, 0, patterns, memory);
+        first += counter != 0;
+        second += counter;
     }
 
     cout << "First " << first << endl;
+    cout << "Second " << second << endl;
     cout << "Time taken: " << (double)(clock() - tStart)/CLOCKS_PER_SEC << endl;
 }
